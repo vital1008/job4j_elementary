9390f04fd14e03c0db44241f91806d8245dad5cf
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-03-26 22:44:13 +0400

    3.1. Загрузка файлов на github [#501650]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args) {
+        System.out.println("Vitaly Bykadorov");
+        System.out.println("15.05.1975");
+
+    }
+}
