d78653a17eb6b3e9b80ef2d15561bdbd4cd7ac65
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-04-02 17:17:03 +0400

    3.1. Загрузка файлов на github [#501650]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -1,12 +1,12 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(double height ) {
-        double rsl = height-100*1.15;
+    public static double manWeight(double height) {
+        double rsl = (height-100)*1.15;
         return rsl;
     }
-    public static double womanWeight(double height) {
-        double rsl= height-110*1.15;
+    public static double womanWeight(double height2) {
+        double rsl= (height2-110)*1.15;
         return rsl;
 
     }
@@ -15,9 +15,9 @@ public class Fit {
         double height = 187;
         double man = Fit.manWeight(height);
         System.out.println("Man 187 is" + man);
-        double height= 175;
-        double height= Fit.womanWeight(height);
-        System.out.println("Woman 175 is");
+        double height2= 175;
+        double woman= Fit.womanWeight(height2);
+        System.out.println("Woman 175 is" + woman);
 
     }
 
--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,10 +1,27 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2){
+    public static double distance(int x1 , int y1, int x2, int y2){
+        double rsl1 =x2-x1;
+        return rsl1;
+        double rsl2= y2-y1;
+        return rsl2;
+        double rsl3 = Math.pow(rsl1, 2);
+        return rsl3;
+        double rsl4 = Math.pow(rsl2, 2);
+        return rsl4;
+        double rsl5 = rsl3 + rsl4;
+        double result = Math.sqrt(rsl5);
+        return result;
+    }
 
-
+    public static void main(String[] args) {
+        double result=Point.distance(0,0,2,0);
+        System.out.println("result (0,0_ to (2,0)");
 
 
     }
 }
+
+
+
