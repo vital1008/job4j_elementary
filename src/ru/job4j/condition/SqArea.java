28f348c8c911342766f4f35c64a08dda0dbbafde
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-04-07 21:27:16 +0400

    Merge remote-tracking branch 'origin/master'

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,15 +1,15 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(double p, double k) {
-        double rsl1=  p / (2 * (k + 1));
-        double rsl2=rsl1*k;
-        double rsl3=rsl2*rsl1;
-        return rsl3;
+    public static double square(int p, double k) {
+        double h=  p / (2 * (k + 1));
+        double l=h*k;
+        double square=l*k;
+        return square;
     }
 
     public static void main(String[] args) {
-        double rsl3=SqArea.square(6,2);
-        System.out.println("p=4, k=2, s=2 = real " + rsl3);
+        double square=SqArea.square(6,2);
+        System.out.println("p=6, k=2, s=1 = real " + square);
     }
 }
