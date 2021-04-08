7c705342774accd2c85c79ce8404a40097dc8923
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-04-08 21:04:10 +0400

    Merge remote-tracking branch 'origin/master'

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea="I love Java";
+        System.out.println(idea);
+        String idea1="But I am a newbie";
+        String idea2=idea+idea1;
+        System.out.println (idea2) ;
+        int year=2021;
+        System.out.println(year);
+    }
+}
