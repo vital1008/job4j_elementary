9956ba5c84cd3437569951ff1d8bdffca4d66565
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-05-02 21:40:14 +0400

    Merge remote-tracking branch 'origin/master'

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 public class ArgMethod {
     public static void hello(String name, int age) {
-        System.out.println("Hello , "  + name  +  -age);
+        System.out.println("Hello , " + name + " " + age);
     }
 
     public static void main(String[] args) {
