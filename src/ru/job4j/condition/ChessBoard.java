47482db1d4a8fe0894b2d2a336cc396d4df37390
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-05-04 20:43:14 +0400

    Merge remote-tracking branch 'origin/master'

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,7 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+
+    }
+}
--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Triangle {
      public static boolean exist(double ab, double ac, double bc) {
-         boolean exist = ab + ac > bc && ac + bc > ab && ab + bc > ac;
-         return false;
+         return ab + ac > bc && ac + bc > ab && ab + bc > ac;
      }
 
     public static void main(String[] args) {
