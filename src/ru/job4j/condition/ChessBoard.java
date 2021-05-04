03bac8341c866b71df81d82855a3eb0c6f077b85
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-05-04 22:07:37 +0400

    Merge remote-tracking branch 'origin/master'

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -2,6 +2,16 @@ package ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-
+        int rsl = 0;
+        if (x1 == y1 || x2 == y2) {
+            rsl = Math.abs(x2 - x1);
+            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+        }
+        return rsl;
     }
+    public static void main(String[] args) {
+        System.out.println(way(0,0,5,5));
+    }
+
 }
+
