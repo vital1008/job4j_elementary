1709ced5ca5934f31154bddc49bc49ee6b3707fc
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-05-17 22:56:40 +0400

    Merge remote-tracking branch 'origin/master'

--- src/ru/job4j/condition/ThreeMax.java
+++ src/ru/job4j/condition/ThreeMax.java
@@ -9,11 +9,15 @@ public class ThreeMax {
         if (second > first && second > third) {
             result = second;
         }
+        if (third > first && third > second) {
+            result = third;
+        }
         return result;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(max(10, 8, 3));
+        System.out.println(max(1, 5, 100));
     }
 }
 
