55bb9a333a5dccf2e8c4d5b0824319028e2225bf
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-05-20 22:26:22 +0400

    Merge remote-tracking branch 'origin/master'

--- test/ru/job4j/condition/ChessBoardTest.java
+++ test/ru/job4j/condition/ChessBoardTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 public class ChessBoardTest {
         @Test
         public void wayIs5() {
-            int x1 = 0;
-            int y1 = 0;
+            int x1 = 6;
+            int y1 = 7;
             int x2 = 1;
-            int y2 = 1;
+            int y2 = 2;
             int result = ChessBoard.way(x1, y1, x2, y2);
-            int expected = 1;
+            int expected = 5;
             Assert.assertEquals(expected, result);
         }
 
