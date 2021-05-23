8758e794ea7696fc5cee8cfd39b8fe0ddd025898
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-05-23 19:55:36 +0400

    Merge remote-tracking branch 'origin/master'

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -11,11 +11,10 @@ public class Counter {
 
     public static int sumByEven(int start, int finish) {
         int sum = 0;
-        for (int i = start; i <= finish; i++){
+        for (int i = start; i <= finish; i++)
             if (i % 2 == 0) {
                 sum = sum + i;
             }
-        }
         return sum;
     }
 
