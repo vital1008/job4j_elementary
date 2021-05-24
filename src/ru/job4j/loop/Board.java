e2caa0e758ab56d4bb08310a7df53e0ab680e678
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-05-24 19:14:03 +0400

    Merge remote-tracking branch 'origin/master'

--- src/ru/job4j/loop/Board.java
+++ src/ru/job4j/loop/Board.java
@@ -2,11 +2,10 @@ package ru.job4j.loop;
 
 public class Board {
     public static void paint(int width, int height) {
-        for (int row = 0; row <= height; row++) {
-            for (int cell = 0; cell <= width; cell++) {
+        for (int row = 0; row < height; row++) {
+            for (int cell = 0; cell < width; cell++) {
                 if ((row + cell) % 2 == 0) {
                     System.out.println("X");
-
                 } else {
                     System.out.print(" ");
                 }
@@ -15,7 +14,6 @@ public class Board {
             System.out.println();
         }
     }
-
     public static void main(String[] args) {
         paint(3, 3);
         System.out.println();
