b857d37ca0e4d0a20fa8adf37ae453c5410b6bc3
Виталий Быкадоров <v.bikadorov@gmail.com>
2021-05-24 21:53:09 +0400

    Merge remote-tracking branch 'origin/master'

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,10 +3,11 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan > nik) {
+        while (ivan < nik) {
             System.out.println(month);
             ivan *= 3;
             nik *= 2;
+            month++;
 
         }
         return month;
